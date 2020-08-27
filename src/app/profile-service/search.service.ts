6b8447cbcaa0052fb08569725ac3a3a5107ebecf
Nyururukelvin <nyururukelvin9@gmail.com>
2020-08-27 22:05:28 +0300

    my commit add Github API

--- src/app/profile-service/search.service.ts
+++ src/app/profile-service/search.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor() { }
+  userName: string;
+  repoName: string;
+  apikey: string = 'f3bed2706dfe69705334dc104eb08f4c6c8d2c82'
+
+  constructor(private http: HttpClient) {
+    console.log('service is now ready');
+
+   }
+
+   
 }
