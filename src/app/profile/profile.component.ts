899d37d7172c9443268858eef36276c66ab171f8
Nyururukelvin <nyururukelvin9@gmail.com>
2020-08-28 11:57:05 +0300

    my commit add method

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../profile-service/search.service'
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  profile: any;
+  repos: any;
+  username: any = 'NyururuKelvin'
+
+  constructor(private profileService:SearchService) {
+
+   }
+
+   findProfile(){
+    this.profileService.updateProfile(this.username);
+
+    this.profileService.getProfileInfo().subscribe(profile =>{
+      console.log(profile);
+    this.profile= profile      
+    });
+
+    this.profileService.getProfileRepos().subscribe(repos =>{
+      console.log(repos);
+    this.repos= repos;      
+    
+    })
+  }
 
   ngOnInit(): void {
   }
